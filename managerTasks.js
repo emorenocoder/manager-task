5bf290579dec2e7eefade9c0a7ddb42586bcef97
emorenocoder <emorenocoder@gmail.com>
2023-07-24 15:58:23 -0400

    Se actualiza y refactoriza script segun material visto en clases

--- managerTasks.js
+++ managerTasks.js
@@ -1,5 +1,6 @@
 let tasks = [];
 let taskString = '';
+
 function addTask(){
     let  task = prompt('Agregue una tarea: ');
     if (task === "" || task === " ") {
@@ -12,94 +13,89 @@ function addTask(){
 
 
 function displayTask() {
-
-    if (tasks.length > 0){
-        for (let i = 0; i < tasks.length; i++) {
-            const element = tasks[i];
-            let estado = 'Pendiente'; 
-            
-            if (element.status == "Completado"){
-                estado = 'Completado';
-            }
-    
-            console.log(`${i+1}.- tarea: ${element.name}, estado: ${estado}`);
-        }
-    } else {
-        console.log('no hay tareas para mostrar');
+   
+    if(tasks.length === 0) {
+        console.log('No hay tareas para mostrar!!')
     }
+    let output = tasks.map( ({name, status}, index) => `${index+1}.- Tarea: ${name} - Estado: ${status}`).join("\n");
     
+    console.log(output);
 }
 
 
 
 function completeTask(){
-    if (tasks.length > 0){
-        let completeDescription = `¿Que tarea desea completar? Ingrese el número de la tarea: \n`;
-        
-        for (let index = 0; index < tasks.length; index++) {
-            const element = tasks[index];
-            completeDescription += `${index+1}.- ${element.name}: ${element.status}\n`;
-        }
 
-        let complete = parseInt(prompt(completeDescription));
-        tasks[complete-1].status = "Completado";
-        console.log(`Tarea ${complete} marcada como completa!!`);
-    }else{
+    if (tasks.length === 0) {
         console.log('no hay tareas para completar');
     }
+
+    let completeDescription = `¿Que tarea desea completar? Ingrese el número de la tarea: \n`;
+
+    tasks.forEach(({name, status}, index) => {
+        if (status != 'Completado'){
+            completeDescription += `${index+1}.- ${name}: ${status}\n`;
+        }
+    });
+
+    let complete = parseInt(prompt(completeDescription));
+    tasks[complete-1].status = "Completado";
+    console.log(`Tarea ${complete} marcada como completa!!`);
+
 }
 
 function removeTask(){
-    
-    if (tasks.length > 0){
-        let removeDescription = `¿Que tarea desea eliminar? Ingrese el numero: \n`;
-        for (let index = 0; index < tasks.length; index++) {
-            const element = tasks[index];
-            removeDescription += `${index+1}.- ${element.name}: ${element.status}\n`;
-        }
-        
-        let remove = parseInt(prompt(removeDescription));
-        tasks.splice(remove - 1, 1);
-        console.log(`Tarea ${remove} eliminada con exito!!`);
-    }else{
-        console.log('no hay tareas para eliminar');
+
+    if (tasks.length === 0) {
+        console.log('No hay tareas para eliminar');
     }
+
+    let removeDescription = `¿Que tarea desea eliminar? Ingrese el numero: \n`;
+   
+    tasks.forEach(({name, status}, index) => {        
+        removeDescription += `${index+1}.- ${name}: ${status}\n`; 
+    });
+
+    let remove = parseInt(prompt(removeDescription));
+    tasks.splice(remove - 1, 1);
+    console.log(`Tarea ${remove} eliminada con exito!!`);
+   
 }
 
+
+
 function manageTask() {
-    
-    let option = '';
-    let descripcion = `===========================
-    Seleccione un numero tarea:\n===========================
+
+    // se elimina el switch y se agrega un objeto para las acciones del menu
+    const taskActions = {
+        '1': addTask,
+        '2': displayTask,
+        '3': completeTask,
+        '4': removeTask
+    };
+   
+    const description = `===========================
+    Seleccione un número de tarea:\n===========================
     1: Agregar tarea
     2: Mostrar tareas
     3: Completar tarea
     4: Eliminar tarea
     5: Salir del programa\n=========================== `;
-    do{
-        option = prompt(descripcion);
-        switch (option) {
-            case '1':
-                addTask();
-                break;
-            case '2':
-                displayTask();
-                break;
-            case '3':
-                completeTask();
-                break;
-            case '4':
-                removeTask();
-                break;
-            case '5':
-                console.log('Ha salido de la aplicacion!!');
-                break;
-            default:
-                console.log('Ingrese un numero entre 1 y 5');
-                break;
+
+    let option = '';
+    do {
+        option = prompt(description);
+        const action = taskActions[option];
+
+        if (action) {
+            action();
+        } else if (option !== '5') {
+            console.log('Por favor, introduzca un número válido entre 1 y 5');
         }
 
-    } while (option != 5);
+    } while (option !== '5');
+
+    console.log('Ha salido de la aplicación!!');
 
 }
 
